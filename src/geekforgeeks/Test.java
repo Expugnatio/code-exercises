e342f27d4a227c122f78deffd7447c40e9d65512
Christiam Hernandez <christiam.alejandro.hernandez@gmail.com>
2019-06-06 22:08:02 -0500

    Rotate Array Problem, with 3 different solutions (Linkedlist, ArrayList, Array)

--- src/geekforgeeks/Test.java
+++ src/geekforgeeks/Test.java
@@ -4,12 +4,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Test {
@@ -17,6 +15,8 @@ public class Test {
 	public static void main(String[] args) {
 		
 		BiFunction<LinkedList<String>, Integer, String> shiftString = (chain, shifts) -> {
+			
+			shifts = (chain.size()-1) - shifts;
 			for (int i = 0; i <= shifts; i++) {
 				chain.addFirst(chain.getLast());
 				chain.removeLast();
@@ -24,13 +24,38 @@ public class Test {
 			return chain.stream().collect(Collectors.joining(" "));
 		};
 		
+		BiFunction<List<String>, Integer, String> shiftStringArray = (chain, shifts) -> {
+			
+			List<String> sub = chain.subList(shifts, chain.size() );
+			List<String> tmp = new ArrayList<>(sub);
+			tmp.addAll(chain.subList(0, shifts));
+			StringBuilder sb = new StringBuilder();
+			tmp.forEach(n -> sb.append(n+" "));
+			return sb.toString().trim();
+		};
+		
+		BiFunction<String[], Integer, String> shiftStringVector = (chain, shifts) -> {
+			StringBuilder sb = new StringBuilder();
+			String[] tmp = new String[chain.length];
+			for (int i = shifts, j = 0; j < tmp.length; i++,j++) {
+				if(i >= tmp.length) {
+					i=0;
+				}
+				tmp[j] = chain[i];
+				sb.append(tmp[j]+" ");
+			}
+			return sb.toString().trim();
+		};
+		
 		File input = new File("test_cases/shift_testcases.txt");
 		
 		LinkedList<String> chainOfNumbers = null;
+		List<String> chainOfNumbers2 = null;
+		Scanner scanner = new Scanner(System.in);
 		//Scanner scanner = new Scanner(System.in);
-		Scanner scanner = null;
 		try {
 			scanner = new Scanner(input);
+			//scanner = new Scanner(System.in);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -43,39 +68,15 @@ public class Test {
 				continue;
 			}else {
 				String[] testData = scanner.nextLine().split("\\s+");
-				chainOfNumbers = new LinkedList<>( Arrays.asList(scanner.nextLine().split("\\s+")) );
-				System.out.println( shiftString.apply(chainOfNumbers, Integer.parseInt(testData[1])) );
+				String strOfNumbers = scanner.nextLine();
+				List<String> listOfNumbers = Arrays.asList(strOfNumbers.split("\\s+"));
+				chainOfNumbers = new LinkedList<>(listOfNumbers);
+				chainOfNumbers2 = new ArrayList<>(listOfNumbers);
+				System.out.println("Linked: "+ shiftString.apply(chainOfNumbers, Integer.parseInt(testData[1])) );
+				System.out.println("Array: " + shiftStringArray.apply(chainOfNumbers2, Integer.parseInt(testData[1])) );
+				System.out.println("Vector: " + shiftStringVector.apply(strOfNumbers.split("\\s+"), Integer.parseInt(testData[1])) );
 			}
 		}
 		scanner.close();
-		
-		
-		
-		
-		
-		System.out.println(shiftString.apply(chainOfNumbers, 2));
-		
-		/*
-		list.add("Nayeem loves counseling</h1><h2>Is Valid</h2>");
-		list.add("<h1>Nayeem loves counseling</h1>");
-		list.add("<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>");
-		list.add("<h1><h1>Sanjay has no watch</h1></h1><Amee>safat codes like a ninja</amee><par>So wait for a while</par>");
-		list.add("<Amee>safat codes like a ninja</amee>");
-		list.add("<SA premium>Imtiaz has a secret crush</SA premium>");
-		
-		list.add("<>hello</>");
-		list.add("<>hello</><h>dim</h>");
-		list.add("<>hello</><h>dim</h>>>>>");*/
-		
-		//currencyFormatter();
 	}
-	
-	public String shiftChain(LinkedList<String> chain, int shifts) {
-		for (int i = 0; i <= shifts; i++) {
-			chain.addFirst(chain.getLast());
-			chain.removeLast();
-		}
-		return chain.stream().collect(Collectors.joining(" "));
-	}
-	
 }
